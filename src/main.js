1b0cc3911cd8aef7bb1e3b97b2be539dcdd0137b
Bobby Bates <robertfbatesjr@gmail.com>
2022-05-15 18:10:04 -0400

    add api key to API_KEY.txt and add that file to .gitignore

--- src/main.js
+++ src/main.js
@@ -4,3 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Hello, World!')
     // console.log(process.env)
 })
+
+fetch('API_KEY.txt')
+    .then(r => r.text())
+    .then(KEY => console.log(KEY))
