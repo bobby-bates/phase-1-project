0c15b6bdd132808cfd8d7786316cae5e89970ebd
Bobby Bates <robertfbatesjr@gmail.com>
2022-05-16 20:46:30 -0400

    get js-api-loader working

--- src/main.js
+++ src/main.js
@@ -1,26 +1,52 @@
 // require('dotenv').config()
+// import { Loader } from '@googlemaps/js-api-loader'
+// import { Loader } from './node_modules/@googlemaps/js-api-loader'
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Hello, World!')
     // console.log(process.env)
+    apiKey()
 })
 
 function apiKey() {
     fetch('API_KEY.txt')
         .then(r => r.text())
-        .then(KEY => console.log(KEY))
-        .catch(error => console.log(error))
+        .then(key => initMap(key))
+        .catch(e => console.error('Fetch error:', e))
 }
-apiKey()
 
-let map
+//=== For INLINE Google Map script loading: ===//
+// let map
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: { lat: 39.832920, lng: -75.151920 },
+//         zoom: 16,
+//     })
+// }
+// window.apiKey = apiKey
+// window.initMap = initMap
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 39.832920, lng: -75.151920 },
-        zoom: 16,
+//=== For DYNAMIC Google Map script loading: ===//
+function initMap(key) {
+    // const loader = new Loader({
+    const loader = new google.maps.plugins.loader.Loader({
+        apiKey: key,
+        version: 'weekly',
+        // libraries: ['places']
     })
-}
 
-window.apiKey = apiKey
-window.initMap = initMap
+    const mapOptions = {
+        center: {
+            lat: 39.832920,
+            lng: -75.151920
+        },
+        zoom: 16
+    }
+
+    loader
+        .load()
+        .then(() => { // google not needed as arg for some reason 🤔
+            new google.maps.Map(document.getElementById('map'), mapOptions)
+        })
+        .catch(e => console.error('Loader error:', e))
+}
