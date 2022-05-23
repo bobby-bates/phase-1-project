432147a868243d6e3d5f040cb7dc7c87ebd0fd3d
Bobby Bates <robertfbatesjr@gmail.com>
2022-05-23 06:44:29 -0400

    completed MVP

--- src/main.js
+++ src/main.js
@@ -26,8 +26,8 @@ function apiKey() {
 // window.apiKey = apiKey
 // window.initMap = initMap
 
-    // Going global with map variable
-    let map
+// Going global with map variable
+let map
 
 //=== For DYNAMIC Google Map script loading: ===//
 function initMap(key) {
@@ -35,7 +35,6 @@ function initMap(key) {
     const centerUS = { lat: 39.833333, lng: -98.583333 }
     const apartment = { lat: 39.832740, lng: -75.152101 }
 
-    // const loader = new Loader({
     // The Loader object constructs the script loading URL with all its params
     const loader = new google.maps.plugins.loader.Loader({
         apiKey: key,
@@ -59,13 +58,12 @@ function initMap(key) {
         .then(() => { // google not needed as arg for some reason 🤔
             // debugger
             map = new google.maps.Map(document.getElementById('map'), mapOptions)
-            new google.maps.Marker({
-                position: apartment,
-                map: map,
-            })
+            // new google.maps.Marker({
+            //     position: apartment,
+            //     map: map,
+            // })
             // debugger
             initAutocomplete(map)
-            new google.maps.Marker()
         })
         .catch(e => console.error('Loader error:', e))
         .finally(() => console.log('🗺💯'))
@@ -87,7 +85,8 @@ function initMap(key) {
                     'name',
                     // 'place_id',
                     // photos returns <= 10 PlacePhoto objects of Place
-                    // 'photos',
+                    // NOTE: No actual photos are returned
+                    'photos',
                     // types returns arr of types for this Place
                     // 'types',
                 ],
@@ -99,7 +98,6 @@ function initMap(key) {
                 },
                 strictBounds: true,
             })
-            const theMap = map
             // debugger
         autocomplete.addListener('place_changed', onPlaceChanged)
     }
@@ -113,21 +111,67 @@ function initMap(key) {
             document.getElementById('autocomplete').placeholder = 'Enter a place'
         } else {
             // debugger
-            // Display details about the valid place
-            document.getElementById('alert').innerText = `Marker for ${placeData.name} added!`
+            document.getElementById('alert').innerText = `Marker for ${placeData.name} added!
+            Click its marker on the map for more info.
+            See below for superb photos of ${placeData.name}.`
             // document.getElementById('details').innerText = JSON.stringify(placeData, null, 2)
-            // Add marker at placeData.location
-            new google.maps.Marker({
-                position: placeData.geometry.location,
-                map: map,
+            
+            
+            const contentStr = 
+            '<div id="content">' +
+            '<div id="siteNotice">' +
+            "</div>" +
+            `<h1 id="firstHeading" class="firstHeading">${placeData.name}</h1>` +
+            "</div>"
+            const infowindow = new google.maps.InfoWindow({
+                content: contentStr,
             })
-            
+            const marker = new google.maps.Marker({
+                position: placeData.geometry.location,
+                title: placeData.name,
+                map,
+            })
+            marker.addListener('click', () => {
+                infowindow.open({
+                    anchor: marker,
+                    map,
+                    shouldFocus: true,
+                })
+            })
+
             // Update viewport using placeData.viewport
-            // map.setBounds(placeData.geometry.viewport)
+            // debugger
+            // console.log(JSON.parse(JSON.stringify(placeData.geometry.viewport)))
+            // autocomplete.setBounds(JSON.parse(JSON.stringify(placeData.geometry.viewport)))
 
-            
-            // STRETCH: Build photo galery with placeData.photos
+            // Make Place Details request:
+            // const request = {
+            //     placeId: placeData.place_id,
+            //     fields: ['photos']
+            // }
+            // const service = new google.maps.places.PlacesService(map)
+            // const callback = (place, status) => {
+            //     if(status === google.maps.places.PlacesServiceStatus.OK){
+            //         console.log('Places Details success!')
+            //         console.log(place)
+            //     }
+            // }
+            // service.getDetails(request, callback)
 
+            // Build photo galery:
+            const gallery = document.getElementById('photo-gallery')
+            placeData.photos.forEach(photo => {
+                // debugger
+                // console.log(photo.getUrl())
+                const img = document.createElement('img')
+                img.id = 'photo'
+                img.src = photo.getUrl({maxWidth: 200, maxHeight: 200})
+                img.innerHTML = photo.html_attributions
+                gallery.appendChild(img)
+            });
+
+            // Reset the text box
+            document.getElementById('autocomplete').placeholder = 'Enter a place'
         }
     }
 }
